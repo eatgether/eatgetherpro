3e5621b80f9a221fdd9f9886d36e81207d16863f
xbthh <hanhan_smith@hotmail.com>
2016-11-11 10:09:40 +0800

    server 出错

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,13 +2,12 @@ class Admin::PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    puts "!!!!!!!!!!!!!!!!!#{@post_2_id}"
+    #puts "!!!!!!!!!!!!!!!!!#{@post_2_id}"
   end
 
   def get_post_2_id
-    @post = Post.find(params[:id])
     @post_2_id = Post.find(params[:id])
-
+    puts "!!!!!!!!!!!!!!!!!#{@post_2_id}"
     redirect_to admin_posts_path(@post_2_id)
   end
 
