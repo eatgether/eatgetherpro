75d9c847bfc6b04d4d3c9afb29ebeb851ac15d21
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-11-14 09:56:31 +0800

    删除错误code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
 
 
   def post_params
-    params.require(:post).permit(:title, :description, :is_hidden)
+    params.require(:post).permit(:title, :description)
   end
 
 end
