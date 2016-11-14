18bdc86b039d926fd0bef28fe8093ec9f9dcf61e
xbthh <hanhan_smith@hotmail.com>
2016-11-14 16:57:58 +0800

    完成邮件功能

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,12 +4,11 @@ class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
     @order = order
     @poster = order.poster.user
-    @asker  = order.asker.user
 
     # puts"!!!!!poster#{@post_user.email}"
     # puts"!!!!!asker#{@ask_user.email}"
 
     mail(to: @poster.email,subject: "[eatgether] 您的预约已匹配。以下是您此次约会详细信息。#{order}")
-    mail(to: @asker.email,subject: "[eatgether] 您的预约已匹配。以下是您此次约会详细信息。#{order}")
+    #mail(to: @asker.email,subject: "[eatgether] 您的预约已匹配。以下是您此次约会详细信息。#{order}")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161114054012) do
-
-
-
 
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
