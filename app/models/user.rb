6935f3bf6929e3bec2867f85b5d51148647f6b31
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-11-14 14:51:33 +0800

    完成用户的详细资料添加和查看

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,20 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  is_admin               :boolean          default(FALSE)
+#  nameChi                :string
+#  nameNick               :string
+#  image                  :string
+#  gender                 :string
+#  birthday               :integer
+#  cellNum                :integer
+#  income                 :integer
+#  heightUser             :integer
 #
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :image, ImageUploader
   scope :all_except, -> (user) {where.not(id: user)}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
