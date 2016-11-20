1dc61fb60a7c56d6e13fcaba46dfb7c6d3053dc1
David <dyz2102@gmail.com>
2016-11-20 18:33:34 +0800

    fixed feedback edit n destroy functions

--- app/controllers/account/order_twos_controller.rb
+++ app/controllers/account/order_twos_controller.rb
@@ -1,8 +1,7 @@
 class Account::OrderTwosController < ApplicationController
   before_action :authenticate_user!
   def index
-    #binding.pry
-    @order_twos = OrderTwo.where(:poster_user_id => current_user) || OrderTwo.where(:asker_user_id => current_user)
+    @order_twos = OrderTwo.where(:poster_user_id => current_user).or(OrderTwo.where(:asker_user_id => current_user))
     #@order_twos = OrderTwo.where(:asker_user_id => current_user)
     #@order_twos = OrderTwo.where(':poster_user_id = ? OR :asker_user_id = ?', current_user ,current_user )
   end
