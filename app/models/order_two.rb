9043ed747c35cc5ceb0784f201c26ef24b9c59b4
qinfeng <q884822@gmail.com>
2016-11-23 14:19:11 +0800

    admin/orders 修改 post/post_id/ask_user_id/都可以点

--- app/models/order_two.rb
+++ app/models/order_two.rb
@@ -17,6 +17,7 @@ class OrderTwo < ApplicationRecord
   belongs_to :asker_user, :class_name => "User"
   belongs_to :post, :class_name => "Post"
   has_many :feedbacks
+  scope :recent, -> {order("created_at DESC")}
 
   aasm do
 		state :order_matched, initial: true
