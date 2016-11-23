70dc30c662d458f7b5683c6e7a2299a7db827aa1
xbthh <hanhan_smith@hotmail.com>
2016-11-23 19:35:22 +0800

    debug

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
 
   def index
-    @notifications = Notification.find_by_recipient_id(current_user.id)
+    @notifications = Notification.where(:recipient_id => current_user.id)
   end
 
 end
