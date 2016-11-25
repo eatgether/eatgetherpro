f4dcc0163cb7966d6b45b14d745438d1aee28556
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-11-25 14:09:27 +0800

    对话功能基本完成，双方都可以发送消息并且看见

--- app/controllers/account/user_conversations_controller.rb
+++ app/controllers/account/user_conversations_controller.rb
@@ -1,21 +1,35 @@
 class Account::UserConversationsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_notification
-  
 
-def show
+
+def index
   @mailbox ||= current_user.mailbox
-
-  @user = User.find(params[:id])
+  @order = OrderTwo.find(params[:order_two_id])
+  if current_user = @order.asker_user
+     @user = @order.poster_user
+  else
+    @user = @order.asker_user
+  end
+  @conversation = @order.conversation
+    @messages = @conversation.messages if @conversation.present?
 end
 
-def update
+def create
   @mailbox ||= current_user.mailbox
-
-  @user = User.find(params[:id])
-
-  conversation = current_user.send_message(@user, params[:body], @user.id ).conversation
-
-  redirect_to account_conversation_path(conversation)
+  #@user = User.find(params[:id])
+  @order = OrderTwo.find(params[:order_two_id])
+  @conversation = @order.conversation
+  if current_user = @order.asker_user
+     @user = @order.poster_user
+  else
+    @user = @order.asker_user
+  end
+  if @conversation.blank?
+    current_user.send_message(@user , params[:body] , @order )
+  else
+    current_user.reply_to_conversation(@conversation, params[:body])
+  end
+   redirect_to account_conversations_path(:order_two_id => @order.id)
 end
 end
