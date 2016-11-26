a1677e3eb30a5f56153f72367290c711363040c2
张硕 <zhangshuo@zhangshuodeMBP.lan>
2016-11-26 20:38:08 +0800

    再次修复bug

--- app/controllers/account/user_conversations_controller.rb
+++ app/controllers/account/user_conversations_controller.rb
@@ -2,34 +2,45 @@ class Account::UserConversationsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_notification
 
-
-def index
-  @mailbox ||= current_user.mailbox
-  @order = OrderTwo.find(params[:order_two_id])
-  if current_user = @order.asker_user
-     @user = @order.poster_user
-  else
-    @user = @order.asker_user
-  end
-  @conversation = @order.conversation
+  def index
+    @mailbox ||= current_user.mailbox
+    @order = OrderTwo.find(params[:order_two_id])
+    if current_user == @order.asker_user
+      @user = @order.poster_user
+    else
+      @user = @order.asker_user
+    end
+    @conversation = @order.conversation
     @messages = @conversation.messages if @conversation.present?
-end
+  end
 
-def create
-  @mailbox ||= current_user.mailbox
-  #@user = User.find(params[:id])
-  @order = OrderTwo.find(params[:order_two_id])
-  @conversation = @order.conversation
-  if current_user = @order.asker_user
-     @user = @order.poster_user
-  else
-    @user = @order.asker_user
-  end
-  if @conversation.blank?
-    current_user.send_message(@user , params[:body] , @order )
-  else
-    current_user.reply_to_conversation(@conversation, params[:body])
-  end
-   redirect_to account_conversations_path(:order_two_id => @order.id)
-end
+  def create
+    @mailbox ||= current_user.mailbox
+    #@user = User.find(params[:id])
+    @order = OrderTwo.find(params[:order_two_id])
+
+    @conversation = @order.conversation
+      if current_user == @order.asker_user
+
+        @user = @order.poster_user
+      else
+        @user = @order.asker_user
+      end
+    if @conversation.blank?
+      @conversation = current_user.send_message(@user , params[:body] , @order.post.title, @order.id).conversation
+      #@conversation.order_two_id = @order.id
+          #binding.pry
+    else
+       @conversation = current_user.reply_to_conversation(@conversation, params[:body])
+    end
+      redirect_to account_conversation_path(@order)
+   end
+
+  # def destroy
+  #   @mailbox ||= current_user.mailbox
+  #   @order = OrderTwo.find(params[:order_two_id])
+  #   @conversation = @order.conversation
+  #   @conversation.destroy
+  # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,25 @@ class User < ApplicationRecord
     nil
   end
   ###
+  def send_message(recipients, msg_body, subject, order, sanitize_text = true, attachment = nil, message_timestamp = Time.now)
+    convo = Mailboxer::ConversationBuilder.new(subject: subject,
+                                               created_at: message_timestamp,
+                                               updated_at: message_timestamp).build
+
+    message = Mailboxer::MessageBuilder.new(sender: self,
+                                            conversation: convo,
+                                            recipients: recipients,
+                                            body: msg_body,
+                                            subject: subject,
+                                            attachment: attachment,
+                                            created_at: message_timestamp,
+                                            updated_at: message_timestamp).build
+
+    convo.order_two_id = order
+    convo.save
+
+    message.deliver false, sanitize_text
+  end
 
   def is_asker_of?(post)
     ask_posts.include?(post)
