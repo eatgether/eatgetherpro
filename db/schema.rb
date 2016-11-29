453f2792ddbdade5c064fea50bba93f6e8080a1b
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-11-29 20:03:45 +0800

    切回分支，数据库迁移

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128013728) do
+ActiveRecord::Schema.define(version: 20161129034151) do
 
   create_table "asker_requests", force: :cascade do |t|
     t.integer  "user_id"
@@ -131,8 +131,11 @@ ActiveRecord::Schema.define(version: 20161128013728) do
   create_table "photos", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "photoable_type"
+    t.integer  "photoable_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "post_restaurants", force: :cascade do |t|
