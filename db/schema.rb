344537829131dd340cd6741e812113d62490f6f5
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-11-30 13:24:46 +0800

    给user添加work的字段

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129034151) do
+ActiveRecord::Schema.define(version: 20161130051417) do
 
   create_table "asker_requests", force: :cascade do |t|
     t.integer  "user_id"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(version: 20161129034151) do
     t.integer  "heightUser"
     t.text     "description"
     t.string   "cellNum"
+    t.string   "work"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
