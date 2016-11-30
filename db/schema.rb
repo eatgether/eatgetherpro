befd32fc97bf5740f9ceba31630e5a76370732ed
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-11-30 13:54:10 +0800

    后台给餐厅添加图片

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130051417) do
+ActiveRecord::Schema.define(version: 20161130053715) do
 
   create_table "asker_requests", force: :cascade do |t|
     t.integer  "user_id"
@@ -161,6 +161,7 @@ ActiveRecord::Schema.define(version: 20161130051417) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
+    t.string   "cellnum"
   end
 
   create_table "user_interests", force: :cascade do |t|
