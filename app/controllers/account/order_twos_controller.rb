ad7c00fd2c03426dcf1eb55a5ff751fa0969ffa8
David <dyz2102@gmail.com>
2016-12-01 13:05:09 +0800

    chg flash to chinese

--- app/controllers/account/order_twos_controller.rb
+++ app/controllers/account/order_twos_controller.rb
@@ -16,9 +16,9 @@ class Account::OrderTwosController < ApplicationController
     if @order_twos.order_matched?
       @order_twos.metMatch!
       OrderMailer.notify_order_met(@order_twos).deliver!
-      flash[:notice] = "Congratulations on making a new friend! We kindly ask your feedback via email: xxx@xxx.com"
+      flash[:notice] = "恭喜您认识一个新朋友~ 请点击‘新建反馈’留下您的美好约饭记忆"
     else
-      flash[:alert] = "Cannot confirm meeting. Please contact admin for support."
+      flash[:alert] = "无法确认邀约，请联系右下方服务人员，感谢您的理解~"
     end
     redirect_to :back
   end
@@ -27,9 +27,9 @@ class Account::OrderTwosController < ApplicationController
     @order_twos = OrderTwo.find(params[:id])
     if @order_twos.aasm_state === "order_matched"
         OrderMailer.notify_admin_cancel(@order_twos).deliver!
-        flash[:warning] = "We have received your cancelltion request. We will process your request as soon as we can."
+        flash[:warning] = "我们已收到您的取消申请，服务人员将会第一时间给予回复"
     else
-        flash[:warning] = "OrderTwos cannot be cancelled. Check if your order status is 'order_matched' or contact our support desk."
+        flash[:warning] = "取消申请错误，请联系右下方服务人员帮助解决，感谢您的理解~"
     end
     redirect_to :back
   end
