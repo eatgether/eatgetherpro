939184101ccc5702937d653ac121e79a979a2700
David <dyz2102@gmail.com>
2016-12-13 22:36:42 +0800

    added text editor CKEditor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201060357) do
+ActiveRecord::Schema.define(version: 20161213211859) do
 
   create_table "asker_requests", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,21 @@ ActiveRecord::Schema.define(version: 20161201060357) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "is_matched", default: false
+  end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
   end
 
   create_table "conversations", force: :cascade do |t|
