6856df1cbe6104bceea5a59b2dfb5297dea23164
David <dyz2102@gmail.com>
2016-12-13 20:49:06 +0800

    added photo to feedback new

--- app/helpers/account/order_twos_helper.rb
+++ app/helpers/account/order_twos_helper.rb
@@ -10,4 +10,14 @@ module Account::OrderTwosHelper
      end
   end
 
+	def render_order_two_index_feedback(order)
+		 if order.post.image.present?
+		 image_tag(order.post.image.thumb.url,style:"width:50%; margin-left:9%")
+		 elsif order.post.restaurant.photos.present?
+		 image_tag(order.post.restaurant.photos[0].avatar.medium.url, style:"width:50%; margin-left:9%")
+		 else
+		 image_tag("https://ooo.0o0.ooo/2016/11/27/583a72fa39c8a.png",style:"width:50%; margin-left:9%")
+		 end
+	end
+
 end
